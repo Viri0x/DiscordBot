f0bad2e4de64620d84c9ef4e778761ae6e4fc5d5
Viri0x <quentin.le-helloco@epita.fr>
2021-03-08 01:28:40 +0100

    Add animations to settle command

--- cogs/mudae.py
+++ cogs/mudae.py
@@ -1,6 +1,7 @@
 import discord
 import random
 import logging
+from time import sleep
 from discord.ext import commands
 
 # Log SetUp
@@ -16,8 +17,19 @@ class Mudae(commands.Cog):
         logging.info("random was " + str(win) + " -> " + args[win].name)
 
         embed = discord.Embed(color=discord.Colour.orange())
-        embed.add_field(name = "The RNG has spoken...".format(args[win].mention), value = args[win].mention + "takes it all !", inline = False)
-        await ctx.send(embed = embed)
+        #embed.add_field(name = "The RNG has spoken...".format(args[win].mention), value = args[win].mention + "takes it all !", inline = False)
+        msg = await ctx.send(embed = embed)
+
+        for i in [":three:", ":two:", ":one:"]:
+            embed = discord.Embed(color=discord.Colour.orange())
+            embed.add_field(name = "The RNG will spoke in...".format(args[win].mention), value = i, inline = False)
+            await msg.edit(embed = embed)
+            sleep(1)
+
+
+        embed = discord.Embed(color=discord.Colour.orange())
+        embed.add_field(name = "The RNG has spoken...".format(args[win].mention), value = ":tada: **" + args[win].mention + "** takes it all ! :tada:", inline = False)
+        await msg.edit(embed = embed)
 
 
 def setup(bot):
