63c57cbde863b063556eadbb667f3ba5426307ef
Viri0x <quentin.le-helloco@epita.fr>
2022-11-09 19:40:03 +0100

    Save pic to imgur preventing 1h erasing from dalle

--- cogs/dalle.py
+++ cogs/dalle.py
@@ -2,6 +2,7 @@ import discord
 import logging
 from discord.ext import commands
 import openai
+from imgurpython import ImgurClient
 
 # Log SetUp
 logging.basicConfig(level=logging.INFO, filename='bot.log', filemode="w")
@@ -11,6 +12,13 @@ openai_file = open("openai_key.txt", "r")
 OPENAI_KEY = openai_file.read()
 logging.info(f"OPENAI_KEY = {OPENAI_KEY}")
 openai.api_key = OPENAI_KEY
+
+# Retrieve IMGUR id and client
+imgid_file = open("imgur_id.txt", "r")
+IMGUR_ID = imgid_file.read()
+imgsct_file = open("imgur_secret.txt", "r")
+IMGUR_SECRET = imgsct_file.read()
+imgur_client = ImgurClient(IMGUR_ID, IMGUR_SECRET)
 
 def send_dalle_request(prompt, size = "256x256", nb_pics = 1):
     """
@@ -56,11 +64,14 @@ class Dalle(commands.Cog):
         logging.info(response)
         image_url = response['data'][0]['url']
 
+        # Upload image to imgur
+        imgur_response = imgur_client.upload_from_url(image_url)
+        logging.info(imgur_response)
 
         # Create Embedded message to display
         embed = discord.Embed(color=discord.Colour.blue())
         embed.add_field(name = f"Your Dall-e creation for the prompt :", value = prompt, inline = False)
-        embed.set_image(url=image_url)
+        embed.set_image(url=imgur_response["link"])
 
         # Display final message
         await ctx.send(embed = embed)
